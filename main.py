d2f42dc37339adde8a48e1a5da48ba4a3412beb1
user <user@example.com>
2025-01-28 15:54:42 -0800

    Implement RecursiveCharacterTextSplitter

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from langchain_community.document_loaders import UnstructuredPDFLoader
 from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 loader = UnstructuredPDFLoader("test.pdf")
 docs = loader.load()
 
-print(docs[0].page_content[0:100])
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=100,
+    chunk_overlap=10,
+    length_function= len,
+)
+
+texts = text_splitter.split_text(docs[0].page_content)
+
+print(texts)
